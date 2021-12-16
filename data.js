565f694f31010bb962ba7162e6b3e7e3dd767db7
Wadly Dugue <dukyw1@gmail.com>
2021-12-16 18:00:19 -0300

    commit con productos agragdos

---
+++ data.js
@@ -0,0 +1,77 @@
+const data = {
+    categories: [
+        {
+            name:'Beverages', 
+            image: '/images/beverages.png',
+        },
+        {
+            name: 'Breakfast',
+            image: '/images/breakfast.jpg',
+        },
+        {
+            name: 'Burgers',
+            image: '/images/burger.png',
+        },
+    ],
+
+    products: [
+        {
+            category: 'Beverages',
+            name: 'Coca-Cola',
+            calorie: 120,
+            price: 100,
+            image: '/images/coca.png',
+        },
+        {
+            category: 'Beverages',
+            name: 'Coca-Cola',
+            calorie: 120,
+            price: 100,
+            image: '/images/fanta.png',
+        },
+        {
+            category: 'Beverages',
+            name: 'Coca-Cola',
+            calorie: 120,
+            price: 100,
+            image: '/images/sprite.png',
+        },
+        {
+            category: 'Breakfast',
+            name: 'Ensalada Cesare',
+            calorie: 120,
+            price: 300,
+            image: '/images/ensalada1.png',
+        },
+        {
+            category: 'Breakfast',
+            name: 'Ensalada Quemadora',
+            calorie: 320,
+            price: 300,
+            image: '/images/ensalada2.png',
+        },
+        {
+            category: 'Burgers',
+            name: 'Menu Grande',
+            calorie: 700,
+            price: 300,
+            image: '/images/hamb1.png',
+        },
+        {
+            category: 'Burgers',
+            name: 'Grande Hamburguesa',
+            calorie: 120,
+            price: 650,
+            image: '/images/hamb2.png',
+        },
+        {
+            category: 'Burgers',
+            name: 'Churras',
+            calorie: 610,
+            price: 300,
+            image: '/images/hamb3.jpg',
+        },
+    ]
+};
+
+module.exports = data;
